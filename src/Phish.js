0f79757c274902feeed6b61060c2a6cd86bec576
Elena Strakhova <elenastrakhova@gmail.com>
2023-01-05 17:39:26 -0500

    second commit

--- src/Phish.js
+++ src/Phish.js
@@ -2,9 +2,9 @@ import { april } from "./april";
 
 function Phish() {
   return (
-    <div className="px-4 py-5 my-5 text-center">
-      <p className="h1 m-5"><span className="opaque">Phish, April 20-24</span></p>
-      <div className='d-flex flex-row flex-wrap justify-content-evenly align-items-center'>
+    <div className="p-4 text-center">
+      <p className="h1 p-5"><span className="opaque">Phish, April 20-24</span></p>
+      <div className='d-flex p-4 flex-row flex-wrap justify-content-evenly align-items-center'>
         {april.map((element => {
           return <img className='item' key={element} src={`/apr/${element}`} alt={element} />
         }))}
--- src/USOpen.js
+++ src/USOpen.js
@@ -2,9 +2,9 @@ import { september } from "./september";
 
 function USOpen() {
   return (
-    <div className="px-4 py-5 my-5 text-center">
-      <p className="h1 m-5"><span className="opaque">US Open, September 7</span></p>
-      <div className='d-flex flex-row flex-wrap justify-content-evenly align-items-center'>
+    <div className="p-4 text-center">
+      <p className="h1 p-5"><span className="opaque">US Open, September 7</span></p>
+      <div className='d-flex p-4 flex-row flex-wrap justify-content-evenly align-items-center'>
           {september.map((element => {
           return <img className='item' key={element} src={`/sep/${element}`} alt={element} />
         }))}
